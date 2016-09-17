264ef6b43a43832107697dd07339d74d23ee33ac
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-17 14:54:57 -0300

    Adding Game,

--- Game.js
+++ Game.js
@@ -1,55 +1,89 @@
-    var stage = new PIXI.Container();
 
-    // create a renderer instance.
-    var screenWidth  = 600
-    var screenHeight = 600
-    var renderer = PIXI.autoDetectRenderer(screenWidth, screenHeight, {backgroundColor : 0x1099bb});
-    var projectPath = "http://localhost:8000/"
+function getTime() {
+    var date = new Date();
+    var now  = date.getTime();
+    return now
+}
+var appStart = getTime()
 
-    function setupGame(){
-        console.log("SETUP GAME");
+var stage = new PIXI.Container();
 
-        // Resizing renderer to center of browser window. Credits: http://www.html5gamedevs.com/topic/18406-how-to-center-stage-on-browser/
-        function resize() {
-            renderer.view.style.position = 'absolute';
-            renderer.view.style.left = ((window.innerWidth - renderer.width) >> 1) + 'px';
-            renderer.view.style.top  = ((window.innerHeight - renderer.height) >> 1) + 'px';
-        } resize();
-        window.addEventListener('resize', resize);
+// create a renderer instance.
+var screenWidth  = 600
+var screenHeight = 600
 
+var centerX = screenWidth/2
+var centerY = screenHeight/2
 
-        // add the renderer view element to the DOM
-        document.body.appendChild(renderer.view);
+var renderer = PIXI.autoDetectRenderer(screenWidth, screenHeight, {backgroundColor : 0x1099bb});
+var projectPath = "http://localhost:8000/"
 
-        // create a texture from an image path
-        var texture = PIXI.Texture.fromImage(projectPath + "bunny.png");
-
-        // create a new Sprite using the texture
-        var bunny = new PIXI.Sprite(texture);
-
-        // center the sprites anchor point
-        bunny.anchor.x = 0.5;
-        bunny.anchor.y = 0.5;
-
-        // move the sprite t the center of the screen
-        bunny.position.x = 200;
-        bunny.position.y = 150;
-
-        stage.addChild(bunny);
-
-        requestAnimationFrame( animate );
-
-        function animate() {
-            requestAnimationFrame( animate );
-
-            // just for fun, lets rotate mr rabbit a little
-            bunny.rotation += 0.1;
-
-            //bunny.position.x += 01;
-
-            // render the stage
-            renderer.render(stage);
-        }
-
-        loader.load();
+function waitForStart() {
+    var now = getTime()
+    console.log(now, appStart, now-appStart)
+    if (now - appStart > 2000) {
+        setupPosLoad()
     }
+    else
+        requestAnimationFrame( waitForStart );
+}
+requestAnimationFrame(waitForStart)
+
+
+
+function setupPosLoad() {
+    var text = new PIXI.Text('This is a text',{fontFamily : 'gameFont', fontSize: 24, align : 'center', });
+    text.position.x = centerX
+    text.position.y = centerY
+    stage.addChild(text)
+}
+
+
+function setupPreLoad() {
+    console.log("SETUP GAME");
+
+    // Resizing renderer to center of browser window. Credits: http://www.html5gamedevs.com/topic/18406-how-to-center-stage-on-browser/
+    function resize() {
+        renderer.view.style.position = 'absolute';
+        renderer.view.style.left = ((window.innerWidth - renderer.width) >> 1) + 'px';
+        renderer.view.style.top  = ((window.innerHeight - renderer.height) >> 1) + 'px';
+    } resize();
+    window.addEventListener('resize', resize);
+
+
+    // add the renderer view element to the DOM
+    document.body.appendChild(renderer.view);
+
+    // create a texture from an image path
+    var texture = PIXI.Texture.fromImage(projectPath + "bunny.png");
+
+    // create a new Sprite using the texture
+    var bunny = new PIXI.Sprite(texture);
+
+    // center the sprites anchor point
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    // move the sprite t the center of the screen
+    bunny.position.x = 200;
+    bunny.position.y = 150;
+
+    stage.addChild(bunny);
+
+    requestAnimationFrame( animate );
+
+    function animate() {
+        requestAnimationFrame( animate );
+        // just for fun, lets rotate mr rabbit a little
+        bunny.rotation += 0.1;
+
+        // render the stage
+        renderer.render(stage);
+    }
+
+    var text = new PIXI.Text('This is a dummy text, dont remove pls', {fontFamily : 'gameFont', fontSize: 24, align : 'center', });
+    stage.addChild(text)
+    text.alpha = 0
+
+    loader.load();
+}
