17dcb0eb37d5940f4866b84c16626ff8fd9a2f80
Epaminondas Neto <epaminondas.neto@tapps.com.br>
2016-09-17 13:03:37 -0300

    loading structure

---
+++ gameLoader.js
@@ -0,0 +1,34 @@
+console.log("loader")
+
+
+function onAssetsLoaded(evt)
+{
+	gameData = evt;
+
+	gameEvents = gameData.resources.events.data;
+	gameQuestions = gameData.resources.questions.data;
+	gameConfig = gameData.resources.config.data;
+
+	///start the game here, all files are loaded
+	showPlayerOptions(0);
+}
+
+//unique instance
+var loader = PIXI.loader;
+
+//static data variables
+var gameQuestions;
+var gameEvents;
+var gameConfig;
+
+//load all JSONS
+loader.add('questions',"configs/gameQuestions.json");
+loader.add('config',"configs/gameConfig.json");
+loader.add('events',"configs/gameEvents.json");
+
+//complete callback
+loader.on('complete',onAssetsLoaded);
+
+//start loading
+loader.load();
+
---
+++ src/view/showPlayerOptions.js
@@ -0,0 +1,4 @@
+function showPlayerOptions(index){
+	console.log("show option of index " + index)
+	console.log(gameQuestions.playerOptions[index].question);
+}
