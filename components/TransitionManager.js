e5220df3c3691a8b8f548f3c53091a665b18354f
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-17 16:59:28 -0300

    Starting work on TransitionManager.

---
+++ components/TransitionManager.js
@@ -0,0 +1,15 @@
+
+// Manager
+var TransitionManager = {}
+
+function startTransition() {
+
+}
+
+
+
+// Initialization
+TransitionManager.registeredTimers = []
+
+// Exposed Functions
+TransitionManager.startTransition = startTransition
