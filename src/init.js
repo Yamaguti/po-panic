62823276e4f536e443bcafeb9b89132ce2ecbc94
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-18 08:33:04 -0300

    removing unused variable.

--- src/init.js
+++ src/init.js
@@ -14,9 +14,6 @@ var screenRight  = screenWidth
 var screenTop    = 0
 var screenLeft   = 0
 
-
-var projectPath = "http://localhost:8000/"
-
 // create a renderer instance.
 var renderer = PIXI.autoDetectRenderer(screenWidth, screenHeight, {backgroundColor : 0x1099bb});
 
