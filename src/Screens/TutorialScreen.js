baba55209bb62b3dbb525565eba24abd25920d4a
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-20 21:36:37 -0300

    Updating Tutorial Screen.

--- src/Screens/TutorialScreen.js
+++ src/Screens/TutorialScreen.js
@@ -2,10 +2,56 @@
 var DEBUGSKIP = false
 var TutorialScreen = {}
 
-var descriptionTextStyle = {fontFamily : 'gameFont', fill: '#EEEEEE',fontSize: 30, align : 'center', wordWrap : true, wordWrapWidth : screenWidth - 80, }
+// Stylesheet
+TutorialScreen.descriptionTextStyle = {fontFamily : 'gameFont', fill: '#EEEEEE',fontSize: 30, align : 'center', wordWrap : true, wordWrapWidth : screenWidth - 80, }
 
 
 TutorialScreen.showTutorial = function(index) {
+    var content = new PIXI.Container();
+    stage.addChild(content)
+
+    TutorialScreen.content = content;
+    TutorialScreen.index = index;
+    Game.pause(true);
+
+    if (DEBUGSKIP) {
+        TutorialScreen.closeTutorial();
+        return;
+    }
+
+    var holder = Utils.newImage({
+        "name": "assets/ChoicesScreen/txt_holder.png",
+    })
+    holder.position.x = centerX,
+    holder.position.y = screenHeight - (holder.height + 70)
+    content.addChild(holder)
+
+    // Text
+    var question = new PIXI.Text(gameTutorials.tutorials[index].label, TutorialScreen.descriptionTextStyle);
+    content.addChild(question)
+    TutorialScreen.question = question
+
+    question.position.x = holder.position.x - question.width/2;
+    question.position.y = holder.position.y - question.height/2;
+
+    holder.interactive = true;
+    holder.on('mousedown', TutorialScreen.nextTutorial);
+}
+
+
+TutorialScreen.nextTutorial = function() {
+    AudioLib.playSFX("assets/Sounds/sfx/button.wav")
+    TutorialScreen.index++;
+
+    if(gameTutorials.tutorials[TutorialScreen.index]) {
+        TutorialScreen.question.text = gameTutorials.tutorials[TutorialScreen.index].label;
+    } else {
+        TutorialScreen.closeTutorial();
+    }
+}
+
+
+TutorialScreen.microManageTutorial = function(index) {
     var content = new PIXI.Container();
     TutorialScreen.content = content;
     TutorialScreen.index = index;
@@ -24,50 +70,7 @@ TutorialScreen.showTutorial = function(index) {
     content.addChild(holder)
 
     // Text
-    TutorialScreen.question = new PIXI.Text(gameTutorials.tutorials[index].label, descriptionTextStyle);
-    content.addChild(TutorialScreen.question)
-    TutorialScreen.question.position.x = holder.position.x - TutorialScreen.question.width/2;
-    TutorialScreen.question.position.y = holder.position.y - TutorialScreen.question.height/2;
-
-    stage.addChild(content)
-
-    TutorialScreen.question.interactive = true;
-    TutorialScreen.question.on('mousedown', TutorialScreen.nextTutorial);
-}
-
-TutorialScreen.nextTutorial = function()
-{
-    AudioLib.playSFX("assets/Sounds/sfx/button.wav")
-    TutorialScreen.index++;
-    if(gameTutorials.tutorials[TutorialScreen.index])
-    {
-        TutorialScreen.question.text = gameTutorials.tutorials[TutorialScreen.index].label;
-    }else{
-        TutorialScreen.closeTutorial();
-    }
-}
-
-
-TutorialScreen.microManageTutorial = function(index) {
-    var content = new PIXI.Container();
-    TutorialScreen.content = content;
-    TutorialScreen.index = index;
-    Game.pause(true);
-
-    // if (DEBUGSKIP) {
-    //     TutorialScreen.closeTutorial();
-    //     return;
-    // }
-
-    var holder = Utils.newImage({
-        "name": "assets/ChoicesScreen/txt_holder.png",
-    })
-    holder.x = centerX;
-    holder.y = screenHeight - (holder.height + 70) ;
-    content.addChild(holder)
-
-    // Text
-    TutorialScreen.question = new PIXI.Text("CLICK AT THE SCREEN LIKE A MAD MAN!\nIt's your time to shine! You have the power to micro manage your team and earn extra revenue!", descriptionTextStyle);
+    TutorialScreen.question = new PIXI.Text("CLICK AT THE SCREEN LIKE A MAD MAN!\nIt's your time to shine! You have the power to micro manage your team and earn extra revenue!", TutorialScreen.descriptionTextStyle);
     content.addChild(TutorialScreen.question)
     TutorialScreen.question.position.x = holder.position.x - TutorialScreen.question.width/2;
     TutorialScreen.question.position.y = holder.position.y - TutorialScreen.question.height/2;
