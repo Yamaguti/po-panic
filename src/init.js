69f72180e753ff6a9f935ad02f038945ac0a97a4
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-20 21:13:41 -0300

    Updating index with comments. Moving constants to constants file.

--- src/init.js
+++ src/init.js
@@ -1,21 +1,12 @@
 
+// Set true to show FPS
 var DEBUGMODE = true
 
+// Set to scale pixel art
 PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 
+// Main Stage
 var stage = new PIXI.Container();
-
-var screenWidth  = 960
-var screenHeight = 540
-
-var centerX = screenWidth/2
-var centerY = screenHeight/2
-
-var screenBottom = screenHeight
-var screenRight  = screenWidth
-var screenTop    = 0
-var screenLeft   = 0
 
 // create a renderer instance.
 var renderer = PIXI.autoDetectRenderer(screenWidth, screenHeight, {backgroundColor : 0x1099bb});
-
